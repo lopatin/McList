675bdd0c0aa861bb3d56d6d2b3299e57375de3ff
Alex Lopatin <aleksander.lopatin@gmail.com>
2012-08-04 02:54:33 -0700

    Working sessions, kind of

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express'),
 	app = express.createServer(),
+	_cookie = require('cookie'),
 	io = require('socket.io').listen(app),
 	RedisStore = require('connect-redis')(express),
 	sessionStore = new RedisStore(),
@@ -10,14 +11,14 @@ var express = require('express'),
  */
 app.configure(function(){
 	app.set('views', __dirname);
+	app.use(express.cookieParser());
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
-	app.use(express.static(__dirname + '/public'));
-	app.use(express.cookieParser());
 	app.use(express.session({
 		secret: "PeopleLoveLists",
 		store: sessionStore
 	}));
+	app.use(express.static(__dirname + '/public'));
 });
 
 /*
@@ -38,8 +39,9 @@ app.register('.html', {
 io.configure(function() {
     io.set('authorization', function(data, callback) {
         if (data.headers.cookie) {
-            var cookie = parseCookie(data.headers.cookie);
+            var cookie = _cookie.parse(data.headers.cookie);
             sessionStore.get(cookie['connect.sid'], function(err, session) {
+            	console.log("FROM SOCKET AUTH: "+session.myVar);
                 if (err || !session) {
                     callback('Error', false);
                 } else {
@@ -55,17 +57,19 @@ io.configure(function() {
 
 app.listen(3333);
 
+var count = 0;
+
 /*
  * Most app logic (socket communication)
  */
 io.sockets.on('connection', function(socket){
-	var session = socket.handshake.session;
+	console.log("FROM SOCKET: "+socket.handshake.session.myVar);
 
 	/*
 	 * socket represents an open socket connection with one browser
 	 */
 	socket.emit('user-info', {
-		session: session
+		session: null
 	});
 
 	socket.on('test', function(data){
@@ -84,5 +88,12 @@ io.sockets.on('connection', function(socket){
 	socket.on('incoming-event', function(data){
 		console.log(data.someMessage);
 	});
+
+	count++;
+});
+
+app.get('/myindex.html', function(req, res){
+	console.log("FROM EXPRESS: "+req.session.myVar);
+	res.send('hi');
 });
 
