6c15e2e508792e3dea2d8cc41812bf30ca8809ce
Alex Lopatin <aleksander.lopatin@gmail.com>
2012-08-04 03:17:13 -0700

    Sessions with socket.io and static file serving only when needed

--- server.js
+++ server.js
@@ -1,37 +1,24 @@
-var express = require('express'),
-	app = express.createServer(),
-	_cookie = require('cookie'),
-	io = require('socket.io').listen(app),
-	RedisStore = require('connect-redis')(express),
-	sessionStore = new RedisStore(),
-	_ =	require('underscore');
+var express = 		require('express'),
+	app = 			express.createServer(),
+	_cookie = 		require('cookie'),
+	io = 			require('socket.io').listen(app),
+	RedisStore = 	require('connect-redis')(express),
+	sessionStore = 	new RedisStore(),
+	_ =				require('underscore'),
+	fs = 			require('fs');
 
 /*
  * Configure the express.js server
  */
 app.configure(function(){
-	app.set('views', __dirname);
 	app.use(express.cookieParser());
 	app.use(express.bodyParser());
-	app.use(express.methodOverride());
 	app.use(express.session({
 		secret: "PeopleLoveLists",
 		store: sessionStore
 	}));
-	app.use(express.static(__dirname + '/public'));
 });
 
-/*
- * Underscore templates
- */
-app.register('.html', {
-  compile: function(str, options){
-    var compiled = require('underscore').template(str);
-    return function(locals) {
-        return compiled(locals);
-    };
-  }
-});
 
 /*
  * Get sessions working with socket.io
@@ -55,9 +42,7 @@ io.configure(function() {
     });
 });
 
-app.listen(3333);
 
-var count = 0;
 
 /*
  * Most app logic (socket communication)
@@ -89,11 +74,27 @@ io.sockets.on('connection', function(socket){
 		console.log(data.someMessage);
 	});
 
-	count++;
 });
 
-app.get('/myindex.html', function(req, res){
-	console.log("FROM EXPRESS: "+req.session.myVar);
-	res.send('hi');
+var count = 0;
+
+/*
+ * Main page render
+ */
+app.get('/', function(req, res){
+	console.log('asdf');
+	if(!(req.session && req.session.clientId)){
+		req.session.clientId = count;
+		count++;
+	}
+
+	console.log("FROM EXPRESS: "+req.session.clientId);
+
+	fs.readFile(__dirname + '/public/myindex.html', 'utf8', function(err, text){
+        res.send(text);
+    });
 });
 
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3333);
