650603e9b6c8540083e17081d22fd84d98d0da50
Alex Lopatin <aleksander.lopatin@gmail.com>
2012-08-03 22:56:53 -0700

    Basic server stuff and example socket.io code

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 var express = require('express'),
-	app = express.createServer();
+	app = express.createServer(),
+	redis = require('redis'),
+	RedisStore = require('connect-redis')(express),
+	sessionStore = new RedisStore(),
+	io = require('socket.io').listen(app);
 
 app.listen(3333);
 
+io.sockets.on('connection', function(socket){
+
+
+
+	/*
+	 * How to use socket io
+	 */
+	socket.emit('test-event', {
+		testdata: 'test'
+	}, function(response){
+		console.log('response from client');
+	});
+
+	socket.on('incoming-event', function(data){
+		console.log(data.someMessage);
+	});
+});
+
 app.get('/', function(req, res){
-	res.send('hi');
+	res.send('testing the index action');
 });
