9a29d7e9699b5d3ae2ef2a74b85686ddf7ba7ef8
Magnus Paaske <magnuspetersenpaaske@gmail.com>
2012-08-04 00:11:05 -0700

    sockets added

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,15 @@
 tpl.loadTemplates(['task', 'taskedit'], function(){
 	window.appRouter = new window.AppRouter();
 	console.log ("hul igennem");
+	
+	window.keyListener = new window.KeyListener();
+	$('body').keyup(function(event){
+		window.keyListener.keystroke(event);
+	});
+	window.socket = window.io.connect("statsonstats.com:3333");
+	window.socket.on ("test",
+		function(resp){
+			console.log (resp);
+		}
+	);
 });
---
+++ public/js/models/cursor.js
@@ -0,0 +1,5 @@
+window.Cursor = Backbone.Model.extend({
+	
+	
+	
+});
